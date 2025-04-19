57612d41b426e3f88f3aa7f4e3bd31830dae2aae
efthaqur <efthaqur@gmail.com>
2025-04-19 23:20:52 +0600

    registered bootstrap.min.css in function.php

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ add_theme_support('title-tag');
 
 //Theme StyleSheet
 function myClassictheme_style(){
+	wp_enqueue_style('myClassic_bootstrap', get_template_directory_uri().'/dist/css/bootstrap/bootstrap.min.css', array(), '5.3.5');
 	wp_enqueue_style('myClassic_style', get_stylesheet_uri(), array(), '1.0.0');
 }
 
