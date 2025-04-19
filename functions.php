2ba15f9ad9c2e7aae8ef54bed1b89946710cd5dc
efthaqur <efthaqur@gmail.com>
2025-04-19 23:56:04 +0600

    bootstrap js file registered in function.php

--- functions.php
+++ functions.php
@@ -9,10 +9,19 @@ function myClassictheme_style(){
 	wp_register_style('bootstrap', get_template_directory_uri().'/dist/css/bootstrap/bootstrap.min.css', array(), '5.3.5', 'all');
 	wp_enqueue_style('bootstrap');
 	wp_enqueue_style('myClassic_style', get_stylesheet_uri(), array(), '1.0.0');
+	
+		
 }
 
 add_action('wp_enqueue_scripts', 'myClassictheme_style');
 
+//Javascript Files
+function myClassictheme_scripts(){
+	wp_enqueue_script('bootstrap.bundle.min', get_template_directory_uri().'/dist/css/bootstrap/bootstrap.min.css', array(), '5.3.5', 'false');
+	
+}
+
+add_action('wp_enqueue_scripts', 'myClassictheme_scripts');
 
 ?>
 
